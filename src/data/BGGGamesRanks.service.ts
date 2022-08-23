2d31c1a715ac3f2868d57579865e999033e21bc8
Max Zinovik <zinovik@gmail.com>
2022-08-23 22:16:19 +0400

    update bgg-games-ranks link

--- src/data/BGGGamesRanks.service.ts
+++ src/data/BGGGamesRanks.service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { DataService } from './DataService.interface';
 import { Data } from '../common/model/Data.interface';
 
-const URL = 'https://bgg-games-ranks-zinovik.vercel.app/api/get-games?amount=100&load';
+const URL = 'https://bgg-games-ranks.vercel.app/api/get-games?amount=100&load';
 
 export class BGGGamesRanksService implements DataService {
   async getData(): Promise<Data> {
